355adcf83ad37b1bf60f8fc0cc0048bf01851a1c
marsgpl <marsgpl@gmail.com>
2020-05-31 21:31:54 +0300

    moar covid keywords

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,16 @@ elif [[ "$CMD" == "util" ]]; then
     if [[ -z "$SKIP_BUILD" ]]; then
         ./deploy/build.sh util || exit 1
     fi
-
     ./deploy/restart.sh util || exit 1
 elif [[ "$CMD" == "crawler" ]]; then
     if [[ -z "$SKIP_BUILD" ]]; then
         ./deploy/build.sh crawler || exit 1
     fi
-
     ./deploy/restart.sh crawler || exit 1
 elif [[ "$CMD" == "api" ]]; then
     if [[ -z "$SKIP_BUILD" ]]; then
         ./deploy/build.sh api || exit 1
     fi
-
     ./deploy/restart.sh api || exit 1
 else
     echo "unknown command: $CMD" 1>&2
